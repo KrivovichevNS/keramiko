86cd25cb360052bb9101c95fe472b9db3aa367f2
NikolayKr <nickdiaz1667@gmail.com>
2023-01-31 14:31:36 +0300

    mkdir&files for product page

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,24 @@ import Navbar from './components/navBar/NB';
 import { Routes, Route } from 'react-router-dom';
 import StorePage from './components/storePage/StorePage';
 import BasketPage from './components/basketPage/BasketPage';
+import ProductPage from './components/storePage/products/ProductPage/ProductPage';
 
 function App() {
 
   return (
     <>
-        <Navbar />
-        <div className='container'>
-          <Routes>
-            <Route path='*' element/>
-            <Route path='/store' element={<StorePage />}>
-              <Route path='/store/:category' element={<StorePage />} />
-            </Route>
-            <Route path='/cart' element={<BasketPage />} />
-          </Routes>
-        </div>
-        {/* <footer className='footer'>by KolaSteklo 2023</footer> */}
+      <Navbar />
+      <div className='container'>
+        <Routes>
+          <Route path='*' element />
+          <Route path='/store' element={<StorePage />}>
+            <Route path='/store/:category' element={<StorePage />} />
+          </Route>
+          <Route path='/product/:id' element={<ProductPage />} />
+          <Route path='/cart' element={<BasketPage />} />
+        </Routes>
+      </div>
+      {/* <footer className='footer'>by KolaSteklo 2023</footer> */}
     </>
 
   );
